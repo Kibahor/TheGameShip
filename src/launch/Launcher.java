83f77eabcef34622b3a8db63941ff23bb30dd01a
lublouin1 <lukas.blouin@etu.uca.fr>
2021-12-03 08:22:25 +0100

    Ajout de la structure de base du projet JavaFX

---
+++ src/launch/Launcher.java
@@ -0,0 +1,20 @@
+package launch;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+    @Override
+    public void start (Stage primaryStage) throws java.io.IOException {
+        Parent racine = FXMLLoader.load(getClass().getResource("/FXML/Fenetre.fxml"));
+        Scene scene=new Scene(racine);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Awesome Window");
+        primaryStage.show();
+
+    }
+}
---
+++ src/view/Fenetre.java
@@ -0,0 +1,27 @@
+package view;
+
+import javafx.scene.paint.Color;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.shape.Rectangle;
+
+public class Fenetre {
+    @FXML
+    private Button bouton1;
+    @FXML
+    private Rectangle rectangle1;
+
+    public void initialize() {}
+
+    @FXML
+    private void Clicked(Event e) {
+        bouton1.setText("Coucou");
+    }
+
+    @FXML
+    private void rec_scroll(Event e){
+        rectangle1.setX(rectangle1.getX()+100);
+        rectangle1.setFill(Color.BEIGE);
+    }
+}
