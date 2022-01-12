6f8b4da209e1f20a882eac91022730e0a1d2c8f0
lublouin1 <lukas.blouin@etu.uca.fr>
2022-01-12 10:31:48 +0100

    Ajout de l'entité depuis le CodeBehind de MainWindow

--- Code/TheGameShip/src/view/MainWindow.java
+++ Code/TheGameShip/src/view/MainWindow.java
@@ -2,6 +2,8 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import launch.Launcher;
 
@@ -10,12 +12,15 @@ public class MainWindow {
     private Pane pane;
     @FXML
     private Circle joueur;
-    @FXML
+
     private Circle obstacle;
 
     public void initialize() throws Exception {
             //C'est au GameManager -> Niveau de bind les bonne propriété
+            obstacle=new Circle(20, Color.DARKGRAY);
             Launcher.gameManager.BindProperties("Vaisseau",joueur.centerXProperty(),joueur.centerYProperty(),joueur.radiusProperty());
             Launcher.gameManager.BindProperties("Obstacle",obstacle.centerXProperty(),obstacle.centerYProperty(),obstacle.radiusProperty());
+            pane.getChildren().add(obstacle);
+
     }
 }
