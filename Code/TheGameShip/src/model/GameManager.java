fad4aa3969f45ad31eb0156297cd642961b70a7a
vaclergue <valentin.clergue@etu.uca.fr>
2022-01-12 12:06:11 +0100

    Changements de valeurs de tests

--- Code/TheGameShip/src/model/GameManager.java
+++ Code/TheGameShip/src/model/GameManager.java
@@ -21,22 +21,25 @@ public class GameManager {
     //ViewManager ?
 
     public GameManager() {
-        entityManager=new EntityManager();
+        entityManager = new EntityManager();
         boucle = new Boucle(50);
         thread = new Thread(boucle);
+        player = new Player("file://test.jpg","Vaisseau",100,360,50,5, 10, 10); //DEBUG
         player=new Player("file://test.jpg","Vaisseau",100,300,40,5, 10, 10); //DEBUG
         entityManager.add(player); //DEBUG
+        entityManager.add(new Entity("file://test.jpg","Obstacle","Obstacle",500,360,100));//DEBUG
         entityManager.add(new Entity("file://test.jpg","Obstacle","Obstacle",300,300,20));//DEBUG
         //TODO:ICI tu abonne une méthode a la boucle
     }
 
     public void start() {
         try {
-            move=new MovePlayer();
-            collider= new Collider();
-            Input input=new Keyboard(this);
+            move = new MovePlayer();
+            collider = new Collider();
+            Input input = new Keyboard(this);
             boucle.subscribe(input); //DEBUG
-        } catch(Exception err) {
+        }
+        catch(Exception err) {
             err.printStackTrace();
         }
         thread.start(); //DEBUG
@@ -54,18 +57,18 @@ public class GameManager {
             y.bind(e.yProperty());
             radius.bind(e.hitbox_radiusProperty());
         }
-        catch(Exception err) {
+        catch (Exception err) {
             err.printStackTrace();
         }
     }
 
-    public void movePlayer(String key){
-                switch (key) {
-                    case "UP", "Z" -> move.up(player,collider);
-                    case "LEFT", "Q" -> move.left(player,collider);
-                    case "DOWN", "S" -> move.down(player,collider);
-                    case "RIGHT", "D" -> move.right(player,collider);
-                    case "SPACE" -> move.shoot(player,collider); //TODO: Faire ça ailleur !
-                }
+    public void movePlayer (String key) {
+        switch (key) {
+            case "UP", "Z" -> move.up(player,collider);
+            case "LEFT", "Q" -> move.left(player,collider);
+            case "DOWN", "S" -> move.down(player,collider);
+            case "RIGHT", "D" -> move.right(player,collider);
+            case "SPACE" -> move.shoot(player,collider); //TODO: Faire ça ailleur !
+        }
     }
 }
