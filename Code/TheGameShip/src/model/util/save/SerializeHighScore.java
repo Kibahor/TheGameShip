7053d400018e33c17833b3780a9a2787b1c003d2
Kibahor <56401976+Kibahor@users.noreply.github.com>
2022-01-20 23:42:08 +0100

    Revert "La persistance fonctionne désormé parfaitement (fuck XML)"
    This reverts commit 16eb181f

--- Code/TheGameShip/src/model/util/save/SerializeHighScore.java
+++ Code/TheGameShip/src/model/util/save/SerializeHighScore.java
@@ -7,20 +7,19 @@ public class SerializeHighScore {
 
     private ArrayList<String> listHighScore =  new ArrayList<>();
 
+    public ArrayList<String> getListHighScore() { return listHighScore; }
     public SerializeHighScore() {
         listHighScore.add("No Score Yet");
     }
     public SerializeHighScore(HighScore highScore) {
+        /*if(highScore.getListScore().isEmpty()){
+             listHighScore.add("No Score Yet");
+        }*/
         listHighScore.addAll(highScore.getListScore());
     }
 
-    public ArrayList<String> getListHighScore() { return listHighScore; }
-    public void setListHighScore(ArrayList<String> l) { this.listHighScore = l; }
-
     @Override
     public String toString() {
-        return "SerializeSettings{" +
-                "listHighScore=" + listHighScore +
-                '}';
+        return super.toString();
     }
 }
